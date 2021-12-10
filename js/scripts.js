5be32cfb69f03b899a66dc00d12f7d241e8dd2aa
zlatanglor <oshovskiy.d@gmail.com>
2021-12-10 17:15:58 +0200

    fixed burger js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 $('.close').click(function () {
     $('.od_promo_banner').slideUp(500);
 })
+
 // burger menu, Oshovskiy Dima
-$(document).ready(function() {
-    $('.burger').click(function() {
-        $('.burger').toggleClass('burger_active');
-        $('header nav, header .main_container').toggleClass('burger_active');
-    });
+$('.burger').click(function() {
+    $('.burger, header nav, header .main_container').toggleClass('burger_active');
 });
+

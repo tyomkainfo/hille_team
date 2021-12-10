53b72b676c4b2722eb2ae516349f8539c32b6a72
zlatanglor <oshovskiy.d@gmail.com>
2021-12-10 17:12:39 +0200

    add burger js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $('.close').click(function () {
     $('.od_promo_banner').slideUp(500);
 })
+// burger menu, Oshovskiy Dima
+$(document).ready(function() {
+    $('.burger').click(function() {
+        $('.burger').toggleClass('burger_active');
+        $('header nav, header .main_container').toggleClass('burger_active');
+    });
+});

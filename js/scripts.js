06f6ac765dfef94058cd50db36c55ed00f48da75
peschanskyi <peschanskyi.ye@gmail.com>
2021-12-12 11:11:24 +0200

    Section Peschanskyi added

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,93 @@ $('.close').click(function () {
 
 // burger menu, Oshovskiy Dima
 $('.burger').click(function() {
-    $('.burger, .nav-bar').toggleClass('burger_active');
+    $('.burger, header nav, header .main_container').toggleClass('burger_active');
 });
-    var str=document.getElementById('printt').innerHTML;
-    var i=str.length;
-
-    var a=0; // Сколько символов показать в самом начале.
-    function text() {if(a<=i) {
-    document.getElementById('printt').innerHTML = str.substring(0, a);
-    a++;
-    setTimeout("text()", 100); // Скорость печатания текста
-}} text();
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// carousel, Peschanskyi
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        mouseDrag: false,
+        touchDrag: false,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            375:{
+                items:1,
+                margin: 0
+            },
+            530:{
+                items:2,
+                margin: 0
+            },
+            767:{
+                items:3,
+                margin: 0
+            },
+            991:{
+                items:4,
+                margin: 0
+            },
+            1199:{
+                items:5,
+                margin: 0
+            },
+            1200:{
+                items:5,
+                margin: 30
+            }
+        }
+    });
+});

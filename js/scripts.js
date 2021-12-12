b12ab8216c182e5c24ea1a924fcf9ba5c846d845
Roman12345671234 <012roman210@gmail.com>
2021-12-12 10:29:43 +0200

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -94,3 +94,41 @@ $(document).ready(function(){
         }
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.close').click(function () {
+    $('.od_promo_banner').slideUp(500);
+})
+
+// burger menu, Oshovskiy Dima
+$('.burger').click(function() {
+    $('.burger, .nav-bar').toggleClass('burger_active');
+});
+var str=document.getElementById('printt').innerHTML;
+var i=str.length;
+
+var a=0; // Сколько символов показать в самом начале.
+function text() {if(a<=i) {
+    document.getElementById('printt').innerHTML = str.substring(0, a);
+    a++;
+    setTimeout("text()", 100); // Скорость печатания текста
+}} text();
